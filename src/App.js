ae7217fb8ce5a6919274770320731af68536e9a2
Kate Fox <katefox100@gmail.com>
2021-03-01 12:57:39 +0000

    Header and Paragraph components working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// this makes it clear that they are components
+import Header from "./Components/Header";
+import Paragraph from "./Components/Paragraph";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <Paragraph />
+  </>
+);
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+// we use className to add classes for Bootstrap styling
+const Header = () => (
+    <header class="jumbotron">
+      <h1>Hello, world!</h1>
+      <p>It's actually amazing</p>
+    </header>
+  );
+  
+  export default Header;
