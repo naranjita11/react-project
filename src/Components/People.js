fb881d0322ced602992118872187f575242650aa
Kate Fox <katefox100@gmail.com>
2021-03-02 13:03:51 +0000

    People component updated with a console.log() when clicked

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,7 +1,7 @@
 const People = ({ names }) => (
   
   names ? (
-    <ul className="people">
+    <ul className="people" onClick={ () => console.log("Hello world!") }>
       { names.map((person, index) => (
         <li className="list-item" key={ index }>{ person }</li>))}
     </ul>) : <p>Nothing to see here</p>
